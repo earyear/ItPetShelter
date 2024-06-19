cd92d703224bc4a808ef394d6d8ee8a22a1a6c6f
cherrye0ng <dlqndl5297@gmail.com>
2024-06-19 17:41:44 +0900

    봉사일정 선택부분 수정

--- src/main/java/com/itpetshelter/itpetshelter/controller/IPSController.java
+++ src/main/java/com/itpetshelter/itpetshelter/controller/IPSController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,16 +16,23 @@ public class IPSController {
     public String base() {
         return "layout/base";
     }
-
+    
+    // 보호소 위치
     @GetMapping("/location")
     public String loaction() {
         return "page/location";
     }
 
+//    봉사활동 신청폼
     @GetMapping("/volunteer")
     public String volunteer() {
         return "page/volunteer";
     }
+//    봉사활동 신청서 처리
+
+
+
+
 
 
 }
